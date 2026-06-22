3d22f31bdaf60d5efd249e49e60f929638005cc7
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-06-22 16:03:29 +0600

    Color Patch

--- config.h
+++ config.h
@@ -1,9 +1,24 @@
 #include "logos/katana.h"
-#define COLOR "\e[1;36m"
+//define colors
+#define BLACK  "\e[1;30m"
+#define RED    "\e[1;31m"
+#define GREEN  "\e[1;32m"
+#define YELLOW "\e[1;33m"
+#define BLUE   "\e[1;34m"
+#define PURPLE "\e[1;35m"
+#define CYAN   "\e[1;36m"
+#define WHITE  "\e[1;37m"
+
+
+//define display colors
+#define COLOR BLUE
+#define LOGOCOLOR CYAN
+#define TITLECOLOR BLUE
 
 #define CONFIG \
 { \
    /* name            function                 cached */\
+    SPACER \
     { "",             get_title,               false }, \
     { "",             get_bar,                 false }, \
     { "OS: ",         get_os,                  true  }, \
@@ -18,7 +33,7 @@
     { "Terminal: ",   get_terminal,          false }, \
     SPACER \
     { "CPU: ",        get_cpu,                 true  }, \
-    { "GPU: ",        get_gpu1,                true  }, \
+    { "GPU: ",        get_gpu1,                true }, \
     { "Memory: ",     get_memory,              false }, \
     SPACER \
     { "",             get_colors1,             false }, \
@@ -40,4 +55,7 @@
 #define GPU_CONFIG \
 { \
     REMOVE("Corporation"), \
+    REMOVE("Controller"), \
+    REMOVE("Graphics"), \
+    REMOVE("Integrated"), \
 }
